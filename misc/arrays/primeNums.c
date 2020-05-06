d9a4554978fc6cca9d60a1b386882b2b10122ab5
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-06 14:14:10 +0100

    fixed primes function

--- misc/arrays/primeNums.c
+++ misc/arrays/primeNums.c
@@ -3,12 +3,39 @@
 int main()
 {
     int range = 99;
-    int primes[99] = {2,3};
+    int primes[55] = {2,3, [54]=0};
+    //first undefined value in the array
+    int currTop = 2;
 
     for(int i=4;i<range;i++){
-            
+        //remove even numbers
+        if(i%2==0){
+            continue;
+        }
+
+        //for odd numbers
+        else{
+            //for all defined values in the array
+            for (int j = 0; j<currTop;j++){
+                if (i%primes[j]==0){
+                    break;
+                }
+                                
+                //if divisible by another prime AND j is the last defined element
+                if ((i%primes[j] != 0)&&(j==currTop-1)){
+                    primes[currTop] = i;
+                    currTop++;
+                    break;
+                }
+                               
+            }
+        }
     }
     
+    for(int i=0;i<55;i++){
+        printf("%d\n", primes[i]);
+    }
+
 
 
     return 0;
