9f400f6be6b65cbf0f91cd69faef5223b305c1b2
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-09 09:51:28 +0100

    added bubbleSort and revString

---
+++ misc/strings/complexStr.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void revString(char letters[]);
+void bubbleSort(char str[][20], int size);
+
+int main(){
+    char string[50];
+    int len=0;
+    printf("How many words are you going to sort?\n");
+    scanf("%d", &len);
+    char list[len][20];
+
+    for (int i = 0;i<len;i++){
+        scanf("%s", &list[i]);
+    }
+    printf("\n\n\n\n\n\n");
+
+    bubbleSort(list, len);
+
+    return 0;
+}
+
+void revString(char letters[]){
+    int len = strlen(letters);
+    char reversed[]= {""};
+    for(int i = len-1, j=0; i>=0; i--,j++){
+        reversed[j]=letters[i];
+    }
+    printf("%s\n", reversed);
+}
+
+void bubbleSort(char str[][20], int size){
+    char tmp[20];
+    int swaps = 0;
+
+    for (int i = 0; i<size-1;i++){
+        if (i == 0){
+            swaps = 0;
+        }
+
+        else if(i == size-1 && swaps==0){
+            break;
+        }
+
+        if(strcmp(str[i], str[i+1]) <= -1){
+            continue;
+        }
+
+        else{
+            swaps+=1;
+            strcpy(tmp, str[i+1]);
+            strcpy(str[i+1], str[i]);
+            strcpy(str[i], tmp);
+            
+        }
+    }
+
+    for (int i = 0;i<size;i++){
+        printf("%s\n",str[i]);
+    }
+
+}
