25919b2088e8d9ae5ecbaaff68514fcce523e99f
jamesoneill997 <jamesoneill997@gmail.com>
2020-05-12 12:10:42 +0100

    added line counter

--- misc/files/getLines.c
+++ misc/files/getLines.c
@@ -6,7 +6,24 @@
 
 
 int main(){
+    FILE *pFile = NULL;
+    int lineCount = 0;
+    char *filename = "test.txt";
+    int c;
+
+    pFile = fopen(filename, "r");
 
 
-    
+
+    while((c=fgetc(pFile)) != EOF){
+        if(c == '\n'){
+            lineCount++;
+        }
+    }
+    fclose(pFile);
+    pFile = NULL;
+
+    printf("%d\n", lineCount+1);
+
+    return 0;
 }
